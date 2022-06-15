ca154b206192ffd214c768515c9ea23967de4545
PedroTFX <pedro.trindade.tfx@gmail.com>
2022-06-15 11:23:08 +0100

    Uncercover Martyn

--- src/com/pidgeonsmssender/sdk/PidgeonSMSSender.java
+++
@@ -1,8 +0,0 @@
-package com.pidgeonsmssender.sdk;
-
-public class PidgeonSMSSender
-{
-    public void send(final String num, final String txt) {
-        System.out.println("{SMSSender} " + num + ": " + txt);
-    }
-}
